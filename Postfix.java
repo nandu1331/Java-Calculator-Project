1401291c0c046b11afbf85558986dce9cd79885d
nanda <nandamonsterking@gmail.com>
2025-02-10 13:35:51 +0530

    implemented postfix evaluation function in the postfix class

--- Postfix.java
+++ Postfix.java
@@ -4,13 +4,13 @@ public class Postfix {
     public static String convertToPostfix (String infixExpression) throws Exception {
         Stack<Character> operatorStack = new Stack<>();
         String postfix = "";
-        char[] tokens = infixExpression.replaceAll("\\s+", "").toCharArray();
+        char[] tokens = infixExpression.replaceAll("\\s+", "").toCharArray(); // Remove spaces
 
         for (char token : tokens) {
             if (Character.isDigit(token) || token == '.') {
                 postfix += token;
             } else {
-                postfix += " ";
+                postfix += " "; // space to seperate numbers
 
                 if (token == '(') {
                     operatorStack.push(token);
@@ -32,5 +32,42 @@ public class Postfix {
                 }
             }
         }
+
+        // Pop remaining operators
+        while (!operatorStack.isEmpty()) {
+            if (operatorStack.peek() == '(') {
+                throw new Exception("Mismatched parentheses");
+            }
+            postfix += " " + operatorStack.pop();
+        }
+
+        return postfix.trim();
+    }
+
+    // Method to evaluate a postfix expression
+    public double evaluatePostfix(String postfix) throws Exception {
+        Stack<Double> stack = new Stack<>();
+        String[] tokens = postfix.split("\\s+"); // Split by spaces
+
+        for (String token : tokens) {
+            if (token.matches("-?\\d+(\\.\\d+)?")) {
+                stack.push(Double.parseDouble(token)); // Push numbers to stack
+            } else if (isOperator(token.charAt(0))) {
+                if (stack.size() < 2) {
+                    throw new Exception("Invalid postfix expression");
+                }
+                double b = stack.pop();
+                double a = stack.pop();
+                stack.push(applyOperator(a, b, token.charAt(0)));
+            } else {
+                throw new Exception("Invalid token: " + token);
+            }
+        }
+
+        if (stack.size() != 1) {
+            throw new Exception("Invalid expression evaluation");
+        }
+
+        return stack.pop();
     }
 }
